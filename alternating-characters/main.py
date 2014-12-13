d4cfad6b27e5481ba468fcb1412aeb76e2b0419b
root <root@dev.(none)>
2014-12-13 15:55:16 +0200

    alternating characters solved

---
+++ alternating-characters/main.py
@@ -0,0 +1,24 @@
+"""
+
+Alternating Cahracters Hackerrank
+
+author: mishavietl
+
+"""
+
+def delete_copies(string):
+    """deletes copies of letters"""
+    amount_delete = 0
+
+    for i in range(len(string) - 1):
+        if string[i] == string[i + 1]:
+            amount_delete += 1
+
+    return amount_delete
+
+def main():
+    """main function"""
+    for x in range(int(raw_input())):
+        print delete_copies(raw_input())
+
+# main()
--- find_digits/main.py
+++ find_digits/main.py
@@ -1,12 +1,13 @@
 """
 
-Main file for find_digits
+Find Digits Hackerrank
 
 author: mishavietl
 
 """
 
 def amount_of_divide_digits(number):
+    """findes digits in number that divides on number"""
     number_s = str(number)
     amount = 0
 
@@ -19,6 +20,7 @@ def amount_of_divide_digits(number):
     return amount
 
 def input_u():
+    """takes input from user"""
     amount_inputs = input()
     numbers = []
     for x in range(amount_inputs):
@@ -26,7 +28,9 @@ def input_u():
     return numbers
 
 def main():
+    """main function"""
     for num in input_u():
         print amount_of_divide_digits(num)
+
 
 main()
